9b305097ee7347155e9fa90ade00e936b782d34d
jennaluz <jpura827@gmail.com>
2023-02-28 15:50:24 -0800

    JP Update file references in profile.php

--- content/profile.php
+++ content/profile.php
@@ -1,7 +1,10 @@
 <?php
 ob_start();
-include 'logic.php';
-include('auth_profile.php');
+//include 'logic.php';
+//include('auth_profile.php');
+
+include "../include/connect.inc.php";
+include "../include/auth_profile.inc.php"
 ?>
 <!DOCTYPE html>
 <!--
@@ -15,7 +18,7 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Other/html.html to edit this temp
         <meta charset="utf-8"><!-- comment -->
         <meta name="viewport" content="width=device-width, intitial-scale=1.0">
         <title>Blog Base e-newspaper</title>
-        <link rel="stylesheet" href="BBlog.css">
+        <link rel="stylesheet" href="./css/styles.css">
         <style>
 
         form {
@@ -59,13 +62,13 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Other/html.html to edit this temp
 
       <nav class="flex-div">
           <div class="nav-left flex-div">
-              <img class="menu-icon" src="menu_icon.png">
-              <a href="index.php"><img src="logo1.png" class="logo" ></a>
+              <img class="menu-icon" src="./images/menu_icon.png">
+              <a href="./index.php"><img src="./images/logo1.png" class="logo" ></a>
           </div>
           <div class="nav-mid flex-div">
               <div class="search-box flex-div">
-                <form action="search-proc.php" method="POST" id="searchForm">
-            <input type="text/submiit" name="search" placeholder="search"/> <img src="search.png">
+                <form action="./search.php" method="POST" id="searchForm">
+            <input type="text/submiit" name="search" placeholder="search"/> <img src="./images/search.png">
         </form>
               </div>
           </div><!-- comment -->
@@ -74,24 +77,24 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Other/html.html to edit this temp
             $loggedInUser = $_SESSION["username"];
             ?>
             <div class="nav-right flex-div">
-                <a href="admin.php"><img src="admin_img.png"></a>
-                <a href="graphDes.php"><img src="gd.png"></a>
-                <a href="create.php"><img src="editor_img.png"></a>
-                <a href="ad_design.php"><img src="ad.png"></a>
-                <a href="user_profile.php" style="padding: 10px"><img src="follow.png"></a>
+                <a href="./admin.php"><img src="./images/admin_img.png"></a>
+                <a href="./design.php"><img src="./images/gd.png"></a>
+                <a href="./create.php"><img src="./images/editor_img.png"></a>
+                <a href="./ads.php"><img src="./images/ad.png"></a>
+                <a href="./profile.php" style="padding: 10px"><img src="./images/follow.png"></a>
                 <u><?php echo $_SESSION['username'] ?></u>
-                <a href="logout.php" style="padding: 10px">Logout</a>
+                <a href="./logout.php" style="padding: 10px">Logout</a>
             <?php
           }else{
             ?>
       <div class="nav-right flex-div">
-          <a href="admin.php"><img src="admin_img.png"></a>
-          <a href="graphDes.php"><img src="gd.png"></a>
-          <a href="create.php"><img src="editor_img.png"></a>
-          <a href="ad_design.php"><img src="ad.png"></a>
-          <a href="user_profile.php"><img src="follow.png"></a>
-          <a href="login.php" >Login</a><!-- comment -->
-          <a href="register.php" style="padding: 10px">Sign Up</a>
+          <a href="./admin.php"><img src="./images/admin_img.png"></a>
+          <a href="./design.php"><img src="./images/gd.png"></a>
+          <a href="./create.php"><img src="./images/editor_img.png"></a>
+          <a href="./ads.php"><img src="./images/ad.png"></a>
+          <a href="./profile.php"><img src="./images/follow.png"></a>
+          <a href="./login.php" >Login</a><!-- comment -->
+          <a href="./register.php" style="padding: 10px">Sign Up</a>
       </div>
       <?php
     }
@@ -101,15 +104,15 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Other/html.html to edit this temp
       <!--------------------- side bar --------------------->
       <div class="sidebar">
           <div class="shortcut-links">
-              <a href="index.php"><img src="home.png"> Home </a></p>
-              <a href="hot.php"><p><img src="hot.png"> Hot! </a></p>
-              <a href="viewSaved.php"><p><img src="saved.png"> Saved </a></p>
-              <a href="archived.php"><p><img src="history.png"> Archived </a></p>
+              <a href="./index.php"><img src="./images/home.png"> Home </a></p>
+              <a href="./hot.php"><p><img src="./images/hot.png"> Hot! </a></p>
+              <a href="./saved.php"><p><img src="./images/saved.png"> Saved </a></p>
+              <a href="./archived.php"><p><img src="./images/history.png"> Archived </a></p>
               <hr>
           </div>
           <div class="Authors">
             <center>
-              <p><a href="social.php">Social</a></p>
+              <p><a href="./social.php">Social</a></p>
             </center>
               <?php
 
@@ -126,7 +129,7 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Other/html.html to edit this temp
                   while($row1 = $getFollowList->fetch_assoc()){
 
                ?>
-                  <a href=""><p><img src="follow.png"><?php echo $row1["username"]; ?></a></p>
+                  <a href=""><p><img src="./images/follow.png"><?php echo $row1["username"]; ?></a></p>
                 <?php
               }
               }
@@ -175,7 +178,7 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Other/html.html to edit this temp
                       $firstname   = "UPDATE users SET fname='$fname' WHERE username='" . $_SESSION["username"] . "'";
                       $result   = mysqli_query($con, $firstname);
                       if($result){
-                        header("Location: user_profile.php");
+                        header("Location: ./profile.php");
                         exit();
                       }else{
                         echo "Error: There was a problem executing the statement...";
@@ -205,7 +208,7 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Other/html.html to edit this temp
                       $lastname   = "UPDATE users SET lname='$lname' WHERE username='" . $_SESSION["username"] . "'";
                       $result1   = mysqli_query($con, $lastname);
                       if($result1){
-                        header("Location: user_profile.php");
+                        header("Location: ./profile.php");
                         exit();
                       }else{
                         echo "Error: There was a problem executing the statement...";
@@ -234,7 +237,7 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Other/html.html to edit this temp
                       $newemail   = "UPDATE users SET email='$email' WHERE username='" . $_SESSION["username"] . "'";
                       $result2   = mysqli_query($con, $newemail);
                       if($result2){
-                        header("Location: user_profile.php");
+                        header("Location: ./profile.php");
                         exit();
                       }else{
                         echo "Error: There was a problem executing the statement...";
@@ -263,7 +266,7 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Other/html.html to edit this temp
                       $newpassword   = "UPDATE users SET password='" . md5($password) . "' WHERE username='" . $_SESSION["username"] . "'";
                       $result3   = mysqli_query($con, $newpassword);
                       if($result3){
-                        header("Location: user_profile.php");
+                        header("Location: ./profile.php");
                         exit();
                       }else{
                         echo "Error: There was a problem executing the statement...";
@@ -289,7 +292,7 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Other/html.html to edit this temp
             }
 ob_end_flush();
           ?>
-          <script src="script.js"></script>
+          <script src="./js/script.js"></script>
 
         </center>
     </body>
