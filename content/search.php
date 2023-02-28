7b9b8bd026d6ba50736d4ec5881d9bf4cdf29591
jennaluz <jpura827@gmail.com>
2023-02-28 15:30:30 -0800

    JP Update file references in search.php

--- content/search.php
+++ content/search.php
@@ -1,24 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
-<?php require('logic.php'); ?>
+<?php require "../include/connect.inc.php"; ?>
 
 <head>
     <meta charset="utf-8">
     <title>Blog - <?php echo $row['postTitle']; ?></title>
-    <link rel="stylesheet" href="BBlog.css">
+    <link rel="stylesheet" href="./css/styles.css">
     <!--<link rel="stylesheet" href="style/main.css">-->
 </head>
 
 <body>
   <nav class="flex-div">
       <div class="nav-left flex-div">
-          <img class="menu-icon" src="menu_icon.png">
-          <a href="index.php"><img src="logo1.png" class="logo" ></a>
+          <img class="menu-icon" src="./images/menu_icon.png">
+          <a href="./index.php"><img src="./images/logo1.png" class="logo" ></a>
       </div>
       <div class="nav-mid flex-div">
           <div class="search-box flex-div">
-            <form action="search-proc.php" method="POST" id="searchForm">
-        <input type="text/submiit" name="search" placeholder="search"/> <img src="search.png">
+            <form action="./search.php" method="POST" id="searchForm">
+        <input type="text/submiit" name="search" placeholder="search"/> <img src="./images/search.png">
     </form>
           </div>
       </div><!-- comment -->
@@ -28,24 +28,24 @@
         $loggedInUser = $_SESSION["username"];
         ?>
         <div class="nav-right flex-div">
-            <a href="admin.php"><img src="admin_img.png"></a>
-            <a href="graphDes.php"><img src="gd.png"></a>
-            <a href="create.php"><img src="editor_img.png"></a>
-            <a href="ad_design.php"><img src="ad.png"></a>
-            <a href="user_profile.php" style="padding: 10px"><img src="follow.png"></a>
+            <a href="./admin.php"><img src="./images/admin_img.png"></a>
+            <a href="./design.php"><img src="./images/gd.png"></a>
+            <a href="./create.php"><img src="./images/editor_img.png"></a>
+            <a href="./ads.php"><img src="./images/ad.png"></a>
+            <a href="./profile.php" style="padding: 10px"><img src="./images/follow.png"></a>
             <u><?php echo $_SESSION['username'] ?></u>
-            <a href="logout.php" style="padding: 10px">Logout</a>
+            <a href="./logout.php" style="padding: 10px">Logout</a>
         <?php
       }else{
         ?>
   <div class="nav-right flex-div">
-      <a href="admin.php"><img src="admin_img.png"></a>
-      <a href="graphDes.php"><img src="gd.png"></a>
-      <a href="create.php"><img src="editor_img.png"></a>
-      <a href="ad_design.php"><img src="ad.png"></a>
-      <a href="user_profile.php"><img src="follow.png"></a>
-      <a href="login.php" >Login</a><!-- comment -->
-      <a href="register.php" style="padding: 10px">Sign Up</a>
+      <a href="./admin.php"><img src="./images/admin_img.png"></a>
+      <a href="./design.php"><img src="./images/gd.png"></a>
+      <a href="./create.php"><img src="./images/editor_img.png"></a>
+      <a href="./ads.php"><img src="./images/ad.png"></a>
+      <a href="./profile.php"><img src="./images/follow.png"></a>
+      <a href="./login.php" >Login</a><!-- comment -->
+      <a href="./register.php" style="padding: 10px">Sign Up</a>
   </div>
   <?php
 }
@@ -55,15 +55,15 @@
   <!--------------------- side bar --------------------->
   <div class="sidebar">
       <div class="shortcut-links">
-          <a href="index.php"><img src="home.png"> Home </a></p>
-          <a href="hot.php"><p><img src="hot.png"> Hot! </a></p>
-          <a href="viewSaved.php"><p><img src="saved.png"> Saved </a></p>
-          <a href="archived.php"><p><img src="history.png"> Archived </a></p>
+          <a href="./index.php"><img src="./images/home.png"> Home </a></p>
+          <a href="./hot.php"><p><img src="./images/hot.png"> Hot! </a></p>
+          <a href="./saved.php"><p><img src="./images/saved.png"> Saved </a></p>
+          <a href="./archived.php"><p><img src="./images/history.png"> Archived </a></p>
           <hr>
       </div>
       <div class="Authors">
         <center>
-          <p><a href="social.php">Social</a></p>
+          <p><a href="./social.php">Social</a></p>
         </center>
           <?php
           session_start();
@@ -80,7 +80,7 @@
               while($row1 = $getFollowList->fetch_assoc()){
 
            ?>
-              <a href=""><p><img src="follow.png"><?php echo $row1["username"]; ?></a></p>
+              <a href=""><p><img src="./images/follow.png"><?php echo $row1["username"]; ?></a></p>
             <?php
           }
           }
@@ -98,7 +98,7 @@
            ?>
       </div>
   </div>
-    <script src="script.js"></script>
+    <script src="./js/script.js"></script>
 </body>
 </html>
 <?php
@@ -128,11 +128,11 @@
                         echo "<td>";
                         echo '<div class="card">';
                         echo '<div class="container">';
-                        echo '<h1><a href="viewpost.php?id=' . $row['postID'] . '">' . $row['postTitle'] . '</a></h1>';
+                        echo '<h1><a href="./post.php?id=' . $row['postID'] . '">' . $row['postTitle'] . '</a></h1>';
                         echo '</div>';
                         echo '<p>Posted on ' . $row['postDate'] . '</p><br>';
                         echo '<p>' . $row['postDesc'] . '</p>';
-                        echo '<p><a href="viewpost.php?id=' . $row['postID'] . '">Read More</a></p>';
+                        echo '<p><a href="./post.php?id=' . $row['postID'] . '">Read More</a></p>';
                         echo '</div>';
                         echo "</td>";
                         $i = $i+1;
