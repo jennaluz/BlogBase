488725dd750ac1114cf45ab3877d7fcb68d28435
jennaluz <jpura827@gmail.com>
2023-02-28 13:58:34 -0800

    JP Update file references in username_wrong.php

--- content/username_wrong.php
+++ content/username_wrong.php
@@ -1,5 +1,5 @@
 <html>
-<?php require('logic.php'); ?>
+<?php require "../include/connect.inc.php"; ?>
     <head>
         <title>Blogbase - Sign Up</title>
         <meta charset="utf-8"><!-- <meta -->
@@ -7,20 +7,20 @@
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/all.css">
          <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-         <link rel="stylesheet" href="BBlog.css">
-                  <link rel="stylesheet" href="enter.css">
+         <link rel="stylesheet" href="./css/styles.css">
+                  <link rel="stylesheet" href="./css/enter.css">
 
     </head>
     <body>
       <nav class="flex-div">
           <div class="nav-left flex-div">
-              <img class="menu-icon" src="menu_icon.png">
-              <a href="index.php"><img src="logo1.png" class="logo" ></a>
+              <img class="menu-icon" src="./images/menu_icon.png">
+              <a href="./index.php"><img src="./images/logo1.png" class="logo" ></a>
           </div>
           <div class="nav-mid flex-div">
               <div class="search-box flex-div">
-                <form action="search-proc.php" method="POST" id="searchForm">
-            <input type="text/submiit" name="search" placeholder="search"/> <img src="search.png">
+                <form action="./search.php" method="POST" id="searchForm">
+            <input type="text/submiit" name="search" placeholder="search"/> <img src="./images/search.png">
         </form>
               </div>
           </div><!-- comment -->
@@ -30,24 +30,23 @@
             $loggedInUser = $_SESSION["username"];
             ?>
             <div class="nav-right flex-div">
-                <a href="admin.php"><img src="admin_img.png"></a>
-                <a href="graphDes.php"><img src="gd.png"></a>
-                <a href="create.php"><img src="editor_img.png"></a>
-                <a href="ad_design.php"><img src="ad.png"></a>
-                <a href="user_profile.php" style="padding: 10px"><img src="follow.png"></a>
+                <a href="./admin.php"><img src="./images/admin_img.png"></a>
+                <a href="./design.php"><img src="./images/gd.png"></a>
+                <a href="./create.php"><img src="./images/editor_img.png"></a>
+                <a href="./ads.php"><img src="./images/ad.png"></a>
+                <a href="./profile.php" style="padding: 10px"><img src="./images/follow.png"></a>
                 <u><?php echo $_SESSION['username'] ?></u>
-                <a href="logout.php" style="padding: 10px">Logout</a>
+                <a href="./logout.php" style="padding: 10px">Logout</a>
             <?php
           }else{
             ?>
       <div class="nav-right flex-div">
-          <a href="admin.php"><img src="admin_img.png"></a>
-          <a href="graphDes.php"><img src="gd.png"></a>
-          <a href="create.php"><img src="editor_img.png"></a>
-          <a href="ad_design.php"><img src="ad.png"></a>
-          <a href="user_profile.php"><img src="follow.png"></a>
-          <a href="login.php" >Login</a><!-- comment -->
-          <a href="register.php" style="padding: 10px">Sign Up</a>
+          <a href="./admin.php"><img src="./images/admin_img.png"></a>
+          <a href="./design.php"><img src="./images/gd.png"></a>
+          <a href="./create.php"><img src="./images/ad.png"></a>
+          <a href="./profile.php"><img src="./images/follow.png"></a>
+          <a href="./login.php" >Login</a><!-- comment -->
+          <a href="./register.php" style="padding: 10px">Sign Up</a>
       </div>
       <?php
     }
@@ -57,15 +56,15 @@
       <!--------------------- side bar --------------------->
       <div class="sidebar">
           <div class="shortcut-links">
-              <a href="index.php"><img src="home.png"> Home </a></p>
-              <a href="hot.php"><p><img src="hot.png"> Hot! </a></p>
-              <a href="viewSaved.php"><p><img src="saved.png"> Saved </a></p>
-              <a href="archived.php"><p><img src="history.png"> Archived </a></p>
+              <a href="./index.php"><img src="./images/home.png"> Home </a></p>
+              <a href="./hot.php"><p><img src="./images/hot.png"> Hot! </a></p>
+              <a href="./saved.php"><p><img src="./images/saved.png"> Saved </a></p>
+              <a href="./archived.php"><p><img src="./images/history.png"> Archived </a></p>
               <hr>
           </div>
           <div class="Authors">
             <center>
-              <p><a href="social.php">Social</a></p>
+              <p><a href="./social.php">Social</a></p>
             </center>
               <?php
               session_start();
@@ -82,7 +81,7 @@
                   while($row1 = $getFollowList->fetch_assoc()){
 
                ?>
-                  <a href=""><p><img src="follow.png"><?php echo $row1["username"]; ?></a></p>
+                  <a href=""><p><img src="./images/follow.png"><?php echo $row1["username"]; ?></a></p>
                 <?php
               }
               }
@@ -104,7 +103,7 @@
       <center>
         <p><h3>That username has already been used.</h3></p>
         <br>
-        <p><h3>Would you like to register <a href="register.php">again?</a><br>Or<br>Would you like to <a href="index.php">return</a> to home?</h3></p>
+        <p><h3>Would you like to register <a href="./register.php">again?</a><br>Or<br>Would you like to <a href="./index.php">return</a> to home?</h3></p>
 
       </center>
     </body>
