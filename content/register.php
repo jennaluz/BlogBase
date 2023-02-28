eca35b310edade5e95c8759e82d48973632070de
jennaluz <jpura827@gmail.com>
2023-02-28 15:43:56 -0800

    JP Update file references in register.php

--- content/register.php
+++ content/register.php
@@ -4,7 +4,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
  */
-require('logic.php');
+//require('logic.php');
+require "../include/connect.inc.php";
 ?>
 
 <html>
@@ -15,20 +16,20 @@ require('logic.php');
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/all.css">
          <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-         <link rel="stylesheet" href="BBlog.css">
-                  <link rel="stylesheet" href="enter.css">
+         <link rel="stylesheet" href="./css/styles.css">
+                  <link rel="stylesheet" href="./css/enter.css">
 
     </head>
     <body>
       <nav class="flex-div">
           <div class="nav-left flex-div">
-              <img class="menu-icon" src="menu_icon.png">
-              <a href="index.php"><img src="logo1.png" class="logo" ></a>
+              <img class="menu-icon" src="./images/menu_icon.png">
+              <a href="./index.php"><img src="./images/logo1.png" class="logo" ></a>
           </div>
           <div class="nav-mid flex-div">
               <div class="search-box flex-div">
-                <form action="search-proc.php" method="POST" id="searchForm">
-            <input type="text/submiit" name="search" placeholder="search"/> <img src="search.png">
+                <form action="./search.php" method="POST" id="searchForm">
+            <input type="text/submiit" name="search" placeholder="search"/> <img src="./images/search.png">
         </form>
               </div>
           </div><!-- comment -->
@@ -38,24 +39,24 @@ require('logic.php');
             $loggedInUser = $_SESSION["username"];
             ?>
             <div class="nav-right flex-div">
-                <a href="admin.php"><img src="admin_img.png"></a>
-                <a href="graphDes.php"><img src="gd.png"></a>
-                <a href="create.php"><img src="editor_img.png"></a>
-                <a href="ad_design.php"><img src="ad.png"></a>
-                <a href="user_profile.php" style="padding: 10px"><img src="follow.png"></a>
+                <a href="./admin.php"><img src="./images/admin_img.png"></a>
+                <a href="./design.php"><img src="./images/gd.png"></a>
+                <a href="./create.php"><img src="./images/editor_img.png"></a>
+                <a href="./ads.php"><img src="./images/ad.png"></a>
+                <a href="./profile.php" style="padding: 10px"><img src="./images/follow.png"></a>
                 <u><?php echo $_SESSION['username'] ?></u>
-                <a href="logout.php" style="padding: 10px">Logout</a>
+                <a href="./logout.php" style="padding: 10px">Logout</a>
             <?php
           }else{
             ?>
       <div class="nav-right flex-div">
-          <a href="admin.php"><img src="admin_img.png"></a>
-          <a href="graphDes.php"><img src="gd.png"></a>
-          <a href="create.php"><img src="editor_img.png"></a>
-          <a href="ad_design.php"><img src="ad.png"></a>
-          <a href="user_profile.php"><img src="follow.png"></a>
-          <a href="login.php" >Login</a><!-- comment -->
-          <a href="register.php" style="padding: 10px">Sign Up</a>
+          <a href="./admin.php"><img src="./images/admin_img.png"></a>
+          <a href="./graphDes.php"><img src="./images/gd.png"></a>
+          <a href="./create.php"><img src="./images/editor_img.png"></a>
+          <a href="./ads.php"><img src="./images/ad.png"></a>
+          <a href="./profile.php"><img src="./images/follow.png"></a>
+          <a href="./login.php" >Login</a><!-- comment -->
+          <a href="./register.php" style="padding: 10px">Sign Up</a>
       </div>
       <?php
     }
@@ -65,15 +66,15 @@ require('logic.php');
       <!--------------------- side bar --------------------->
       <div class="sidebar">
           <div class="shortcut-links">
-              <a href="index.php"><img src="home.png"> Home </a></p>
-              <a href="hot.php"><p><img src="hot.png"> Hot! </a></p>
-              <a href="viewSaved.php"><p><img src="saved.png"> Saved </a></p>
-              <a href="archived.php"><p><img src="history.png"> Archived </a></p>
+              <a href="./index.php"><img src="./images/home.png"> Home </a></p>
+              <a href="./hot.php"><p><img src="./images/hot.png"> Hot! </a></p>
+              <a href="./saved.php"><p><img src="./images/saved.png"> Saved </a></p>
+              <a href="./archived.php"><p><img src="./images/history.png"> Archived </a></p>
               <hr>
           </div>
           <div class="Authors">
             <center>
-              <p><a href="social.php">Social</a></p>
+              <p><a href="./social.php">Social</a></p>
             </center>
               <?php
               session_start();
@@ -90,7 +91,7 @@ require('logic.php');
                   while($row1 = $getFollowList->fetch_assoc()){
 
                ?>
-                  <a href=""><p><img src="follow.png"><?php echo $row1["username"]; ?></a></p>
+                  <a href=""><p><img src="./images/follow.png"><?php echo $row1["username"]; ?></a></p>
                 <?php
               }
               }
@@ -113,7 +114,8 @@ require('logic.php');
         </div>
         <br><br>
         <?php
-        require('logic.php');
+        //require('logic.php');
+        require "../include/connect.inc.php";
         if(isset($_REQUEST['username'])){
             $username = stripslashes($_REQUEST['username']);
             $username = mysqli_real_escape_string($con, $username);
@@ -135,7 +137,7 @@ require('logic.php');
         $check_select = mysqli_query($con, $check);
         $random_name = mysqli_num_rows($check_select);
         if($random_name > 0){
-          header("Location: username_wrong.php");
+          header("Location: ./username_wrong.inc.php");
         }else{
 
         $query    = "INSERT into `users` (username, fname, lname, email, password, Admin, graphic_des, writer, reader, advr)
@@ -144,7 +146,7 @@ require('logic.php');
 
             if ($result) {
                 echo "<div><h3>Registered successfully.</h3></div>";
-                header("Location: login.php");
+                header("Location: ./login.php");
             }
             else{
                 echo "<div><h3>Required fields are missing...</h3></div>";
@@ -202,10 +204,10 @@ require('logic.php');
         ?>
         <br>
     <center>
-      <h3>Already have an account? <a href="login.php">login</a> here.</h3>
+      <h3>Already have an account? <a href="./login.php">login</a> here.</h3>
       <br>
-      <h3>Not what you wanted to see? Go <a href="index.php">back</a> to home.</h3>
+      <h3>Not what you wanted to see? Go <a href="./index.php">back</a> to home.</h3>
     </center>
-    <script src="script.js"></script>
+    <script src="./js/script.js"></script>
     </body>
 </html>
