af36ac58d0aeacac8ca4d1dd7f051415789f151a
jennaluz <jpura827@gmail.com>
2023-02-28 16:18:37 -0800

    JP Update typo in login.php

--- content/login.php
+++ content/login.php
@@ -4,7 +4,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
  */
-require('logic.php');
+//require('logic.php');
+require "../include/connect.inc.php";
 ?>
 
 <html>
@@ -15,18 +16,18 @@ require('logic.php');
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/all.css">
          <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-          <link rel="stylesheet" href="BBlog.css">
-        <link rel="stylesheet" href="enter.css">
+          <link rel="stylesheet" href="./css/styles.css">
+        <link rel="stylesheet" href="./css/enter.css">
     </head><!-- comment -->
     <body>
       <nav class="flex-div">
           <div class="nav-left flex-div">
               <img class="menu-icon" src="menu_icon.png">
-              <a href="index.php"><img src="logo1.png" class="logo" ></a>
+              <a href="./index.php"><img src="logo1.png" class="logo" ></a>
           </div>
           <div class="nav-mid flex-div">
               <div class="search-box flex-div">
-                <form action="search-proc.php" method="POST" id="searchForm">
+                <form action="./search.php" method="POST" id="searchForm">
             <input type="text/submiit" name="search" placeholder="search"/> <img src="search.png">
         </form>
               </div>
@@ -37,24 +38,24 @@ require('logic.php');
             $loggedInUser = $_SESSION["username"];
             ?>
             <div class="nav-right flex-div">
-                <a href="admin.php"><img src="admin_img.png"></a>
-                <a href="graphDes.php"><img src="gd.png"></a>
-                <a href="create.php"><img src="editor_img.png"></a>
-                <a href="ad_design.php"><img src="ad.png"></a>
-                <a href="user_profile.php" style="padding: 10px"><img src="follow.png"></a>
+                <a href="./admin.php"><img src="admin_img.png"></a>
+                <a href="./design.php"><img src="gd.png"></a>
+                <a href="./create.php"><img src="editor_img.png"></a>
+                <a href="./ads.php"><img src="ad.png"></a>
+                <a href="./profile.php" style="padding: 10px"><img src="follow.png"></a>
                 <u><?php echo $_SESSION['username'] ?></u>
-                <a href="logout.php" style="padding: 10px">Logout</a>
+                <a href="./logout.php" style="padding: 10px">Logout</a>
             <?php
           }else{
             ?>
       <div class="nav-right flex-div">
-          <a href="admin.php"><img src="admin_img.png"></a>
-          <a href="graphDes.php"><img src="gd.png"></a>
-          <a href="create.php"><img src="editor_img.png"></a>
-          <a href="ad_design.php"><img src="ad.png"></a>
-          <a href="user_profile.php"><img src="follow.png"></a>
-          <a href="login.php" >Login</a><!-- comment -->
-          <a href="register.php" style="padding: 10px">Sign Up</a>
+          <a href="./admin.php"><img src="admin_img.png"></a>
+          <a href="./design.php"><img src="gd.png"></a>
+          <a href="./create.php"><img src="editor_img.png"></a>
+          <a href="./ads.php"><img src="ad.png"></a>
+          <a href="./profile.php"><img src="follow.png"></a>
+          <a href="./login.php" >Login</a><!-- comment -->
+          <a href="./register.php" style="padding: 10px">Sign Up</a>
       </div>
       <?php
     }
@@ -64,15 +65,15 @@ require('logic.php');
       <!--------------------- side bar --------------------->
       <div class="sidebar">
           <div class="shortcut-links">
-              <a href="index.php"><img src="home.png"> Home </a></p>
-              <a href="hot.php"><p><img src="hot.png"> Hot! </a></p>
-              <a href="viewSaved.php"><p><img src="saved.png"> Saved </a></p>
-              <a href="archived.php"><p><img src="history.png"> Archived </a></p>
+              <a href="./index.php"><img src="home.png"> Home </a></p>
+              <a href="./hot.php"><p><img src="hot.png"> Hot! </a></p>
+              <a href="./saved.php"><p><img src="saved.png"> Saved </a></p>
+              <a href="./archived.php"><p><img src="history.png"> Archived </a></p>
               <hr>
           </div>
           <div class="Authors">
             <center>
-              <p><a href="social.php">Social</a></p>
+              <p><a href="./social.php">Social</a></p>
             </center>
               <?php
 
@@ -108,7 +109,8 @@ require('logic.php');
           </div>
       </div>
         <?php
-            require('logic.php');
+            //require('./logic.php');
+          require "../include/connect.inc.php";
             if(isset($_POST['username'])){
                 $username = stripslashes($_REQUEST['username']);
                 $username = mysqli_real_escape_string($con, $username);
@@ -120,10 +122,10 @@ require('logic.php');
                 $rows = mysqli_num_rows($result);
                 if($rows == 1) {
                     $_SESSION['username'] = $username;
-            header("Location: index.php");
+            header("Location: ./index.php");
                 }
                 else{
-                  header("Location: login_error.php");
+                  header("Location: ./login_error.php");
                 }
             }
             else{
@@ -148,14 +150,14 @@ require('logic.php');
     </center>
     <br>
     <center>
-        <h3>Don't have an account? <br> Click <a href="register.php">here </a>to register.</h3>
+        <h3>Don't have an account? <br> Click <a href="./register.php">here </a>to register.</h3>
         <br>
-        <h3>Not what you wanted? <br> Click <a href="index.php">here</a> to return to home.</h3>
+        <h3>Not what you wanted? <br> Click <a href="./index.php">here</a> to return to home.</h3>
     </center>
         <?php
             }
             ?>
-            <script src="script.js"></script>
+            <script src="./js/script.js"></script>
 
     </body>
 </html>
