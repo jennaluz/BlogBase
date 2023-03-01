71422d2eaa7599e5e74de2d5cb51c8805d65f51c
Logan Finley <finley4.logan@gmail.com>
2023-02-28 16:27:11 -0800

    LF Fix image links in ads.php

--- content/ads.php
+++ content/ads.php
@@ -22,13 +22,13 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Other/html.html to edit this temp
 
       <nav class="flex-div">
           <div class="nav-left flex-div">
-              <img class="menu-icon" src="menu_icon.png">
-              <a href="./index.php"><img src="logo1.png" class="logo" ></a>
+              <img class="menu-icon" src="./images/menu_icon.png">
+              <a href="./index.php"><img src="./images/logo1.png" class="logo" ></a>
           </div>
           <div class="nav-mid flex-div">
               <div class="search-box flex-div">
                 <form action="./search.php" method="POST" id="searchForm">
-            <input type="text/submiit" name="search" placeholder="search"/> <img src="search.png">
+            <input type="text/submit" name="search" placeholder="search"/> <img src="./images/search.png">
         </form>
               </div>
           </div><!-- comment -->
@@ -37,22 +37,22 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Other/html.html to edit this temp
             $loggedInUser = $_SESSION["username"];
             ?>
             <div class="nav-right flex-div">
-                <a href="./admin.php"><img src="admin_img.png"></a>
-                <a href="./design.php"><img src="gd.png"></a>
-                <a href="./create.php"><img src="editor_img.png"></a>
-                <a href="./ads.php"><img src="ad.png"></a>
-                <a href="./profile.php" style="padding: 10px"><img src="follow.png"></a>
+                <a href="./admin.php"><img src="./images/admin_img.png"></a>
+                <a href="./design.php"><img src="./images/gd.png"></a>
+                <a href="./create.php"><img src="./images/editor_img.png"></a>
+                <a href="./ads.php"><img src="./images/ad.png"></a>
+                <a href="./profile.php" style="padding: 10px"><img src="./images/follow.png"></a>
                 <u><?php echo $_SESSION['username'] ?></u>
                 <a href="./logout.php" style="padding: 10px">Logout</a>
             <?php
           }else{
             ?>
       <div class="nav-right flex-div">
-          <a href="./admin.php"><img src="admin_img.png"></a>
-          <a href="./design.php"><img src="gd.png"></a>
-          <a href="./create.php"><img src="editor_img.png"></a>
-          <a href="./ads.php"><img src="ad.png"></a>
-          <a href="./profile.php"><img src="follow.png"></a>
+          <a href="./admin.php"><img src="./images/admin_img.png"></a>
+          <a href="./design.php"><img src="./images/gd.png"></a>
+          <a href="./create.php"><img src="./images/editor_img.png"></a>
+          <a href="./ads.php"><img src="./images/ad.png"></a>
+          <a href="./profile.php"><img src="./images/follow.png"></a>
           <a href="./login.php" >Login</a><!-- comment -->
           <a href="./register.php" style="padding: 10px">Sign Up</a>
       </div>
@@ -64,10 +64,10 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Other/html.html to edit this temp
       <!--------------------- side bar --------------------->
       <div class="sidebar">
           <div class="shortcut-links">
-              <a href="./index.php"><img src="home.png"> Home </a></p>
-              <a href="./hot.php"><p><img src="hot.png"> Hot! </a></p>
-              <a href="./saved.php"><p><img src="saved.png"> Saved </a></p>
-              <a href="./archived.php"><p><img src="history.png"> Archived </a></p>
+              <a href="./index.php"><img src="./images/home.png"> Home </a></p>
+              <a href="./hot.php"><p><img src="./images/hot.png"> Hot! </a></p>
+              <a href="./saved.php"><p><img src="./images/saved.png"> Saved </a></p>
+              <a href="./archived.php"><p><img src="./images/history.png"> Archived </a></p>
               <hr>
           </div>
           <div class="Authors">
@@ -89,7 +89,7 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Other/html.html to edit this temp
                   while($row1 = $getFollowList->fetch_assoc()){
 
                ?>
-                  <a href=""><p><img src="follow.png"><?php echo $row1["username"]; ?></a></p>
+                  <a href=""><p><img src="./images/follow.png"><?php echo $row1["username"]; ?></a></p>
                 <?php
               }
               }
