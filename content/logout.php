b291d8557af60e0bb899750d2f36d384d8cb1385
Logan Finley <finley4.logan@gmail.com>
2023-03-05 12:59:59 -0800

    LF Correct some syntax in logout.php

--- content/logout.php
+++ content/logout.php
@@ -1,43 +1,27 @@
 <?php
-//include 'logic.php';
 include "../include/connect.inc.php";
 ?>
 <!DOCTYPE html>
-<!--
-Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
-Click nbfs://nbhost/SystemFileSystem/Templates/Other/html.html to edit this template
--->
-
-
 <html lang="eng">
     <head>
-        <meta charset="utf-8"><!-- comment -->
+        <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, intitial-scale=1.0">
         <title>Blog Base e-newspaper</title>
         <link rel="stylesheet" href="./css/styles.css">
     </head>
-
     <body>
       <nav class="flex-div">
         <?php include "./views/header.php" ?>
       </nav>
-
-      <!--------------------- side bar --------------------->
       <div class="sidebar">
         <?php include "./views/sidebar.php" ?>
       </div>
-      <!--------------------- side bar --------------------->
-
-        <br><br>
-
-        <!-- This is where the info telling the user they logged out will take place -->
+      <br><br>
         <center>
           <h3><p>You have logged out successfully.</p></h3>
           <br>
           <h3><p>Would you like to return to the <a href="./index.php">home</a> page?</p></h3>
         </center>
-
     </body>
     <script src="./js/script.js"></script>
-
 </html>
