311f65ac897b5aaaa7bb3f8994bb72d0383fc482
Logan Finley <finley4.logan@gmail.com>
2023-03-07 17:18:15 -0800

    LF Redesign admin page UI (#6)

--- content/admin.php
+++ content/admin.php
@@ -1,134 +1,153 @@
 <?php
-require "../include/connect.inc.php";
-include "../include/auth_session.inc.php";
+/*
+ * Logan Finley
+ */
+require_once "../include/connect.inc.php";
+include_once "../include/auth_session.inc.php";
 ?>
+
 <!DOCTYPE html>
-<!--
-Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
-Click nbfs://nbhost/SystemFileSystem/Templates/Other/html.html to edit this template
--->
-
-
-<html lang="eng">
+<html lang="en">
     <head>
-        <meta charset="utf-8"><!-- comment -->
-        <meta name="viewport" content="width=device-width, intitial-scale=1.0">
-        <title>Blog Base e-newspaper</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, intitial-scale=1">
+
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
         <link rel="stylesheet" href="./css/styles.css">
-        <script src="./js/script.js"></script>
+
+        <title>BlogBase</title>
     </head>
 
     <body>
+        <nav class="flex-div">
+            <?php include "./views/header.php" ?>
+        </nav>
+        <div class="sidebar">
+            <?php include "./views/sidebar.php" ?>
+        </div>
 
-      <nav class="flex-div">
-        <?php include "./views/header.php" ?>
-      </nav>
+        <div class="container">
+            <?php
+            // check if the current user has admin authorization
+            $current_user = $_SESSION['username'];
+            $test_auth_query = "SELECT username
+                                FROM users
+                                WHERE username='$current_user' AND Admin=1";
+            $test_auth_result = mysqli_query($con, $test_auth_query);
 
-      <div class="sidebar">
-        <?php include "./views/sidebar.php" ?>
-      </div>
+            // if the user does have admin authorization
+            if ($test_auth_result->num_rows > 0) {
+            ?>
 
-          <br><br><br>
-            <center>
+                <div class="container mt-5 mb-3 text-center">
+                    <h1 class="display-3">Hello <?php echo $_SESSION['username'] ?>,</h1>
+                </div>
+                <ul class="nav nav-tabs" id="myTab" role="tablist">
+                    <li class="nav-item" role="presentation">
+                        <button class="nav-link active" id="registered-tab" data-bs-toggle="tab" data-bs-target="#registered-tab-pane" type="button" role="tab" aria-controls="registered-tab-pane" aria-selected="true">Registered Users</button>
+                    </li>
+                    <li class="nav-item" role="presentation">
+                        <button class="nav-link" id="unregistered-tab" data-bs-toggle="tab" data-bs-target="#unregistered-tab-pane" type="button" role="tab" aria-controls="unregistered-tab-pane" aria-selected="false">Unregistered Users</button>
+                    </li>
+                </ul>
 
-                <p><h1>Hello <?php echo $_SESSION['username'] ?>,<br><br> </h1>
-                <?php
-                //this is the place that sets up the authentification for the page in tandom with auth_session.php
-                  $current_user = $_SESSION['username'];
-                  $test_auth = "SELECT username FROM users where username='$current_user' and Admin=1";
-                  $help = $con->query($test_auth);
-                  if($help->num_rows > 0){
-                    //the rest of the statement is at the bottom and applies if the user doesn't have the proper
-                    //access to the page. If they dont they are not able to see any of the information
-                    ?>
-                <h3>View users who are in the database:</h3>
-                <table style="border-spacing: 15px">
-                <?php
-                    $sql = "SELECT userid, username, fname, lname, email, is_approved, Admin, graphic_Des, writer, reader, advr FROM users WHERE is_approved=1";
-                    $result = $con->query($sql);
-                    if($result->num_rows > 0) {
+                <div class="tab-content" id="myTabContent">
 
-                    echo "<tr>";
-                    echo    "<td>ID:</td>";
-                    echo    "<td>Username:</td>";
-                    echo    "<td>Name:</td>";
-                    echo    "<td>Email:</td>";
-                    echo    "<td>Approved:</td>";
-                    echo    "<td>Admin:</td>";
-                    echo    "<td>Graphic Designer:</td>";
-                    echo    "<td>Writer:</td>";
-                    echo    "<td>Reader:</td>";
-                    echo    "<td>Advritiser:</td>";
-                    echo    "<td>Delete:</td>";
-                    echo "</tr>";
+                    <div class="tab-pane fade show active" id="registered-tab-pane" role="tabpanel" aria-labelledby="registered-tab" tabindex="0">
+                        <?php
+                        $reg_users_query = "SELECT userid, username, fname, lname, email, is_approved, Admin, graphic_Des, writer, reader, advr
+                                            FROM users
+                                            WHERE is_approved=1";
+                        $reg_users_result = mysqli_query($con, $reg_users_query);
+                        $reg_users_array = $reg_users_result->fetch_all(MYSQLI_BOTH);
+                        ?>
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th scope="col">ID</th>
+                                    <th scope="col">Username</th>
+                                    <th scope="col">First Name</th>
+                                    <th scope="col">Last Name</th>
+                                    <th scope="col">Email</th>
+                                    <th scope="col">Approved</th>
+                                    <th scope="col">Admin</th>
+                                    <th scope="col">Designer</th>
+                                    <th scope="col">Writer</th>
+                                    <th scope="col">Reader</th>
+                                    <th scope="col">Advertiser</th>
+                                    <th scope="col">Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php for ($i = 0; $i < $reg_users_result->num_rows; $i++) { ?>
+                                    <tr>
+                                        <?php for ($j = 0; $j < $reg_users_result->field_count; $j++) {  ?>
+                                            <td>
+                                                <?php echo $reg_users_array[$i][$j]; ?>
+                                            </td>
+                                        <?php } ?>
+                                            <td>
+                                                <a href="../include/delete_user.inc.php?userid=<?php echo $reg_users_array[$i]["userid"]; ?>" class="btn btn-outline-danger">Delete</a>
+                                            </td>
+                                    </tr>
+                                <?php } ?>
+                            </tbody>
+                        </table>
+                    </div>
 
+                    <div class="tab-pane fade" id="unregistered-tab-pane" role="tabpanel" aria-labelledby="unregistered-tab" tabindex="0">
+                        <?php
+                        $unreg_users_query = "SELECT userid, username, fname, lname, email, is_approved, Admin, graphic_Des, writer, reader, advr FROM users WHERE is_approved!=1";
+                        $unreg_users_result = mysqli_query($con, $unreg_users_query);
+                        ?>
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th scope="col">ID</th>
+                                    <th scope="col">Username</th>
+                                    <th scope="col">First Name</th>
+                                    <th scope="col">Last Name</th>
+                                    <th scope="col">Email</th>
+                                    <th scope="col">Approved</th>
+                                    <th scope="col">Admin</th>
+                                    <th scope="col">Designer</th>
+                                    <th scope="col">Writer</th>
+                                    <th scope="col">Reader</th>
+                                    <th scope="col">Advertiser</th>
+                                    <th scope="col">Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php for ($i = 0; $i < $unreg_users_result->num_rows; $i++) { ?>
+                                    <tr>
+                                        <?php for ($j = 0; $j < $unreg_users_result->field_count; $j++) {  ?>
+                                            <td>
+                                                <?php echo $unreg_users_array[$i][$j] ?>
+                                            </td>
+                                        <?php } ?>
+                                            <td>
+                                                <a href="../include/approve_user.inc.php?userid=<?php echo $unreg_users_array[$i]["userid"]; ?>" class="btn btn-outline-primary">Approve</a>
+                                                <a href="../include/delete_user.inc.php?userid=<?php echo $unreg_users_arry[$i]["userid"]; ?>" class="btn btn-outline-danger">Delete</a>
+                                            </td>
+                                    </tr>
+                                <?php } ?>
+                            </tbody>
+                        </table>
+                    </div>
 
-                            while($row = $result->fetch_assoc()){
-                                echo "<tr><td> " . $row["userid"] . "</td><td>" . $row["username"] . " </td><td> " . $row["fname"] . " " . $row["lname"] . " </td><td> " . $row["email"] . "</td><td> " .$row["is_approved"] . "</td><td>" . $row["Admin"] . " </td><td> " . $row["graphic_Des"] . " </td><td> " . $row["writer"] . " </td><td> " . $row["reader"] . " </td><td> " . $row["advr"] . " </td>";
-                                ?>
-                    <td><a href="../include/delete_post.inc.php?userid=<?php echo $row["userid"]; ?>">Delete</a></td><!-- comment -->
-                    <?php
-                    echo "</tr><br>";
-                            }
-                        }else{
-                            echo "<tr><td>0 results</td></tr>";
-                        }
-                    ?>
-                </table>
-                <br>
-                <h3>View users who are attempting to register:</h3><br>
-                <table style="border-spacing: 15px">
-                    <?php
-                    $sql1 = "SELECT userid, username, fname, lname, email, is_approved, Admin, graphic_Des, writer, reader, advr FROM users WHERE is_approved!=1";
-                    $result1 = $con->query($sql1);
-                    if($result1->num_rows > 0) {
-                echo "<tr>";
-                    echo    "<td>ID:</td>";
-                    echo    "<td>Username:</td>";
-                    echo    "<td>Name:</td>";
-                    echo    "<td>Email:</td>";
-                    echo    "<td>Approved:</td>";
-                    echo    "<td>Admin:</td>";
-                    echo    "<td>Graphic Designer:</td>";
-                    echo    "<td>Writer:</td>";
-                    echo    "<td>Reader:</td>";
-                    echo    "<td>Advritiser:</td>";
-                    echo    "<td>Delete:</td>";
-                    echo    "<td>Approve:</td>";
-                    echo "</tr>";
+                </div>
 
-                            while($row = $result1->fetch_assoc()){
-                                echo "<tr><td> " . $row["userid"] . "</td><td>" . $row["username"] . " </td><td> " . $row["fname"] . " " . $row["lname"] . " </td><td> " . $row["email"] . "</td>";
-                                echo "<td> " .$row["is_approved"] . "</td><td>" . $row["Admin"] . " </td><td> " . $row["graphic_Des"] . " </td><td> " . $row["writer"] . " </td><td> " . $row["reader"] . " </td><td> " . $row["advr"] . " </td>";
-                                ?>
-                    <td><a href="../include/delete_post.inc.php?userid=<?php echo $row["userid"]; ?>">Delete</a></td><!-- comment -->
-                    <td><a href="../include/approve_user.inc.php?userid=<?php echo $row["userid"]; ?>">Approve</a></td>
-                    <?php
-                    echo "</tr><br>";
-                            }
-                        }else{
-                          echo "<tr><td>0 results</td></tr>";
-                        }
-                    ?>
-
-                </table>
-            </p>
-            <br><!-- comment -->
-
-</center><!-- comment -->
-<?php
-}else{
-  ?>
-  <center>
-    <p><h3>You do not have access to the admin page.</h3></p>
-    <br>
-    <p><h3>Return to <a href="./index.php">Home?</a></h3></p>
-  </center>
-  <?php
-}
-
-?>
-<script src="./js/script.js"></script>
-
-</body>
+            <?php
+            }
+            else {
+            ?>
+                <div class="container mt-5 mb-3 text-center">
+                    <h3 class="display-7">You do not have access to the admin page.</h3>
+                    <h3 class="display-7">Return <a href="./index.php">Home?</a></h3>
+                </div>
+    <?php } ?>
+</div>
+    <script src="./js/script.js"></script>
+    </body>
 </html>
