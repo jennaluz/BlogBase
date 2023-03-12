634de18454bd2553433929b5ee5091af486521e2
jennaluz <jpura827@gmail.com>
2023-03-12 15:06:53 -0700

    JP Update to require in auth_profile.inc.php

--- include/auth_profile.inc.php
+++ include/auth_profile.inc.php
@@ -1,9 +1,9 @@
 <?php
     session_start();
+
     if(!isset($_SESSION["username"])) {
         header("Location: ../content/profile.php");
         header("Location: ../content/need_to_sign_in.php");
         exit();
     }
-
 ?>
