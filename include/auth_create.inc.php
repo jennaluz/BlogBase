30c4a478b717097c81098aab2a74f60777919744
jennaluz <jpura827@gmail.com>
2023-03-12 15:07:47 -0700

    JP Update spacing in auth_create.inc.php

--- include/auth_create.inc.php
+++ include/auth_create.inc.php
@@ -1,9 +1,9 @@
 <?php
     session_start();
+
     if(!isset($_SESSION["username"])) {
         header("Location: ../content/create.php");
         header("Location: ../content/need_to_sign_in.php");
         exit();
     }
-
 ?>
