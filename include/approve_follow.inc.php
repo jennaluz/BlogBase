9cfd0d4c1a6a67b8a86ffbce86904ed549805e5d
jennaluz <jpura827@gmail.com>
2023-03-12 15:13:31 -0700

    JP Update database references in approve_follow.inc.php (#13)

--- include/approve_follow.inc.php
+++ include/approve_follow.inc.php
@@ -1,7 +1,7 @@
 <?php
 require_once "./connect.inc.php";
 
-//this is to update someone from follow to following
+// this is to update someone from follow to following
 $sql = "UPDATE `Followers`
         SET follow_id = 0
         WHERE unique_number = '" . $_GET["unique_number"] . "'";
