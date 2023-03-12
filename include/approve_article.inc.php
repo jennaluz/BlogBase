9a9d78294a03da5cf70e367d22381a3fe7de50a1
jennaluz <jpura827@gmail.com>
2023-03-12 15:15:01 -0700

    JP Update database references in approve_article.inc.php (#13)

--- include/approve_article.inc.php
+++ include/approve_article.inc.php
@@ -1,8 +1,10 @@
 <?php
-//include_once('logic.php');
-include_once "./connect.inc.php";
+require_once "./connect.inc.php";
 
-$sql = "UPDATE blog_posts SET is_approved=1 WHERE postID='" . $_GET["postID"] . "'";
+$sql = "UPDATE `Articles`
+        SET approved = 1
+        WHERE article_id = '" . $_GET["postID"] . "'";
+
 if (mysqli_query($con, $sql)) {
     echo "Record updated successfully";
     header("Location: ../content/design.php");
