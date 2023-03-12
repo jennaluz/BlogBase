804d1f0583a0646b0fcecf0e50d1f8a6a620690e
jennaluz <jpura827@gmail.com>
2023-03-12 14:34:17 -0700

    JP Update database references in action.inc.php (#13)

--- include/action.inc.php
+++ include/action.inc.php
@@ -1,22 +1,21 @@
 <?php
-//include_once('logic.php');
 include_once "./connect.inc.php";
 
-//require "./connect.inc.php";
-
 if(isset($_REQUEST['replyCont'])){
-  $loggedInUser = $_SESSION["username"];
-  //the above is for redirection purposes
-  $COMMENTID = $realCommentId;
-  //get the comment id for the comment that we are replying to
-  $replyCont = $_REQUEST['replyCont'];
-  $content5 = $realContent;
-  $newreply = "INSERT INTO `replys` (username, commentid, replyCont, content)
-                VALUES ('$loggedInUser', '$COMMENTID', '$replyCont', '$content5')";
-  $resultReply = mysqli_query($con, $newreply);
-  if($resultReply){
-    header("Location: ../content/post.php?id=$realPostID");
-  }else{
-    echo "<h4>There was an error submitting the query...</h4>";
-  }
+    // redirection purposes
+    $loggedInUser = $_SESSION["username"];
+
+    // get the comment id for the comment that we are replying to
+    $COMMENTID = $realCommentId;
+    $replyCont = $_REQUEST['replyCont'];
+    $content5 = $realContent;
+    $newreply = "INSERT INTO `Comments` (username, comment_id, parent_id, content)
+                 VALUES ('$loggedInUser', '$COMMENTID', '$replyCont', '$content5')";
+    $resultReply = mysqli_query($con, $newreply);
+
+    if ($resultReply) {
+        header("Location: ../content/post1.php?id=$realPostID");
+    } else {
+        echo "<h4>There was an error submitting the query...</h4>";
+    }
 ?>
