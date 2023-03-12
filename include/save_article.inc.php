0e81ca3b2dbe07b423069399ee6a1e707834b440
jennaluz <jpura827@gmail.com>
2023-03-12 14:56:07 -0700

    JP Update database references in save_article.inc.php (#13)

--- include/save_article.inc.php
+++ include/save_article.inc.php
@@ -1,14 +1,17 @@
 <?php
-//include_once('logic.php');
-
 include_once "./connect.inc.php";
 
 $realSaveID = $_GET["saveID"];
-$sql = "UPDATE save SET is_saved=1 WHERE saveID='" . $_GET["saveID"] . "'";
+$sql = "UPDATE `SavedArticles`
+        SET saved = 1
+        WHERE saved_id = '" . $_GET["saveID"] . "'";
+
 if (mysqli_query($con, $sql)) {
-    $info = "SELECT postID FROM save WHERE saveID='$realSaveID'";
+    $info = "SELECT article_id FROM `SavedArticles`
+            WHERE saveID = '$realSaveID'";
     $getInfo = $con->query($info);
     $realPostID = $getInfo->fetch_assoc();
+
     header("Location: ../content/post1.php?id=" . $realPostID["postID"] . "");
 } else {
     echo "Error updating record: " . mysqli_error($con);
