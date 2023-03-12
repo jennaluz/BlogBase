22710675c0433537cc51bd839dfbf878c453babe
jennaluz <jpura827@gmail.com>
2023-03-12 15:11:27 -0700

    JP Update database references in approve_unfollow.inc.php (#13)

--- include/approve_unfollow.inc.php
+++ include/approve_unfollow.inc.php
@@ -1,12 +1,15 @@
 <?php
-//include_once('logic.php');
-include_once "./connect.inc.php";
-$sql = "UPDATE social_follow SET follow_id=1 WHERE unique_number='" . $_GET["unique_number"] . "'";
+require_once "./connect.inc.php";
+
+//this is to update someone from following to follow
+$sql = "UPDATE `Followers`
+        SET follow_id = 1
+        WHERE unique_number = '" . $_GET["unique_number"] . "'";
+
 if (mysqli_query($con, $sql)) {
     echo "Record updated successfully";
     header("Location: ../content/social.php");
 } else {
     echo "Error updating record: " . mysqli_error($con);
 }
-//this is to update someone from following to follow
 ?>
