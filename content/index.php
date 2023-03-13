516995886c82bea5625e9697591e27fcc5e72f4d
jennaluz <jpura827@gmail.com>
2023-03-13 12:55:04 -0700

    JP Update database references in index.php (#13)

--- content/index.php
+++ content/index.php
@@ -39,11 +39,11 @@
                     echo "<center><td>";
                     echo '<div class="card">';
                     echo '<div class="container">';
-                    echo '<h1><a href="./post1.php?id=' . $row['postID'] . '">' . $row['postTitle'] . '</a></h1>';
+                    echo '<h1><a href="./post1.php?id=' . $row['article_id'] . '">' . $row['title'] . '</a></h1>';
                     echo '</div>';
-                    echo '<p>Posted on ' . $row['postDate'] . '</p><br>';
-                    echo '<p>' . $row['postDesc'] . '</p>';
-                    echo '<p><a href="./post1.php?id=' . $row['postID'] . '">Read More</a></p>';
+                    echo '<p>Posted on ' . $row['submit_date'] . '</p><br>';
+                    echo '<p>' . $row['description'] . '</p>';
+                    echo '<p><a href="./post1.php?id=' . $row['article_id'] . '">Read More</a></p>';
                     echo '</div>';
                     echo "</td></center>";
 
