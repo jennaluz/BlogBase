83f5884426c0946e8586695145ea163ac457b641
jennaluz <jpura827@gmail.com>
2023-03-13 12:53:37 -0700

    JP Update database references in index.php (#13)

--- content/index.php
+++ content/index.php
@@ -1,4 +1,4 @@
-<?php include "../include/connect.inc.php"; ?>
+<?php require_once "../include/connect.inc.php"; ?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -7,7 +7,6 @@
         <meta name="viewport" content="width=device-width, intitial-scale=1">
 
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
         <link rel="stylesheet" href="./css/styles.css">
 
@@ -18,47 +17,55 @@
         <div class="header">
             <?php include "./views/header.php" ?>
         </div>
-
         <div class="sidebar">
             <?php include "./views/sidebar.php" ?>
         </div>
 
-<center>
-        <?php
-       try {
-         $stmt = $con->query('SELECT postID, postTitle, postDesc, postDate FROM blog_posts WHERE is_approved=1 ORDER BY postID DESC');
-         ?><table class="main-space"><?php
-         $i =0;
-         $stmt->fetch_assoc();
-         foreach($stmt as $row) {
+        <center>
+            <?php
+            try {
+                $stmt = $con->query('SELECT article_id, title, description, submit_date
+                                     FROM `Article`
+                                     WHERE approved = 1
+                                     ORDER BY article_id DESC');
+            ?>
+                <table class="main-space">
 
-             echo "<center><td>";
-             echo '<div class="card">';
-             echo '<div class="container">';
-             echo '<h1><a href="./post1.php?id=' . $row['postID'] . '">' . $row['postTitle'] . '</a></h1>';
-             echo '</div>';
-             echo '<p>Posted on ' . $row['postDate'] . '</p><br>';
-             echo '<p>' . $row['postDesc'] . '</p>';
-             echo '<p><a href="./post1.php?id=' . $row['postID'] . '">Read More</a></p>';
-             echo '</div>';
-             echo "</td></center>";
-             $i = $i+1;
-             if($i % 3 == 0 )
-             {
-                 echo "<tr></tr>";
-                 echo "<tr></tr>";
-             }
-           }
-       } catch (PDOException $e) {
-           echo $e->getMessage();
-       }echo "</table>";
-       ?>
+                <?php
+                $i =0;
+                $stmt->fetch_assoc();
 
-   </div>
-   </div>
-   </center>
-   <script src="./js/script.js"></script>
+                foreach($stmt as $row) {
+                    echo "<center><td>";
+                    echo '<div class="card">';
+                    echo '<div class="container">';
+                    echo '<h1><a href="./post1.php?id=' . $row['postID'] . '">' . $row['postTitle'] . '</a></h1>';
+                    echo '</div>';
+                    echo '<p>Posted on ' . $row['postDate'] . '</p><br>';
+                    echo '<p>' . $row['postDesc'] . '</p>';
+                    echo '<p><a href="./post1.php?id=' . $row['postID'] . '">Read More</a></p>';
+                    echo '</div>';
+                    echo "</td></center>";
 
+                    $i = $i+1;
 
+                    if ($i % 3 == 0 ) {
+                        echo "<tr></tr>";
+                        echo "<tr></tr>";
+                    }
+                }
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+
+        echo "</table>";
+        ?>
+
+        </div>
+        </div>
+        </center>
+
+        <script src="./js/script.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
     </body>
 </html>
