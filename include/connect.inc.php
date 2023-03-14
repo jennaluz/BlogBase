990f1d12566e92fd4f414305b51c0352f20e1816
Logan Finley <finley4.logan@gmail.com>
2023-03-14 14:35:29 -0700

    LF Fix issue where long posts aren't able to be created

--- include/connect.inc.php
+++ include/connect.inc.php
@@ -9,16 +9,17 @@ if (!$con) {
     echo "<h3 class='container bg-dark text-center p-3 text-warning rounded-lg mt-5'>not able to establish database connection</h3>";
 }
 
-$sql = "SELECT * 
+$sql = "SELECT *
         FROM `Articles`";
 $query = mysqli_query($con, $sql);
 
 if (isset($_REQUEST["new_post"])) {
     $title = $_REQUEST["title"];
     $content = $_REQUEST["editor1"];
+    $description = substr($content, 0, 253);
 
     $sql = "INSERT INTO `Articles` (title, description, content)
-            VALUES ('$title','$content','$content')";
+            VALUES ('$title','$description','$content')";
     mysqli_query($con, $sql);
 }
 ?>
