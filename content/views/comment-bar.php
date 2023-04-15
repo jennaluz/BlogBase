79c2c460a0649c9dea77e65ad9c1e69731e4f746
Logan Finley <finley4.logan@gmail.com>
2023-04-14 20:51:03 -0700

    LF Added template article comment section

--- content/views/comment-bar.php
+++ content/views/comment-bar.php
@@ -1,11 +1,131 @@
 <div class="offcanvas offcanvas-end bg-light" data-bs-scroll="true" tabindex="-1" id="offcanvas-comments" aria-labelledby="offcanvas-comments-label">
-  <div class="offcanvas-header">
-    <h5 class="offcanvas-title" id="offcanvas-comments-label">Responses</h5>
-    <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-  </div>
-  <div class="offcanvas-body">
-    <div>
-        Comments will appear here
+    <div class="offcanvas-header">
+        <h5 class="offcanvas-title" id="offcanvas-comments-label">Comments (1k+)</h5>
+        <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
     </div>
-  </div>
+    <div class="offcanvas-body">
+        <div>
+            <div class="card mb-2">
+                <div class="d-flex m-3">
+                    <img src="uploads/profile_pictures/anonymous.jpg" class="me-3 rounded-circle" style="width:45px; height:45px;">
+                    <div class="body">
+                        <h5 class="fw-bold">Anonymous</h5>
+                        <textarea class="form-control border-0 p-0" id="user-comment-box" placeholder="Add a comment..." style="resize: none; box-shadow: none"></textarea>
+                    </div>
+                </div>
+                <button class="m-3 ms-auto col-2 btn btn-outline-dark rounded-pill py-1 p-2">Submit</button>
+            </div>
+            <hr>
+            <div class="d-flex">
+                <img src="uploads/profile_pictures/anonymous.jpg" class="me-3 rounded-circle" style="width:45px; height:45px;">
+                <div class="body">
+                    <h5 class="fw-bold">
+                        Anonymous
+                        <small class="text-muted">April 14, 2023</small>
+                    </h5>
+                    <p class="mb-0">
+                        Wow, this is an awesome article!
+                    </p>
+                    <div class="mb-1">
+                        <button class="btn p-1">
+                            <span class="fa-regular fa-heart">
+                        </button>
+                        <button class="btn p-1">Reply</button>
+                    </div>
+                    <div class="d-flex">
+                        <img src="uploads/profile_pictures/anonymous.jpg" class="me-3 rounded-circle" style="width:45px; height:45px;">
+                        <div class="body">
+                            <h5 class="fw-bold">
+                                Anonymous
+                                <small class="text-muted">April 14, 2023</small>
+                            </h5>
+                            <p class="mb-0">
+                                You got that right!
+                            </p>
+                            <div class="mb-1">
+                                <button class="btn p-1">
+                                    <span class="fa-regular fa-heart">
+                                </button>
+                                <button class="btn p-1">Reply</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="d-flex my-2">
+                <img src="uploads/profile_pictures/anonymous.jpg" class="me-3 rounded-circle" style="width:45px; height:45px;">
+                <div class="body">
+                    <h5 class="fw-bold">
+                        Anonymous
+                        <small class="text-muted">April 14, 2023</small>
+                    </h5>
+                    <p class="mb-0">
+                        Wow, this is an awesome article!
+                    </p>
+                    <div class="mb-1">
+                        <button class="btn p-1">
+                            <span class="fa-regular fa-heart">
+                        </button>
+                        <button class="btn p-1">Reply</button>
+                    </div>
+                </div>
+            </div>
+            <div class="d-flex">
+                <div class="mx-3 ps-1 vr"></div>
+                <img src="uploads/profile_pictures/anonymous.jpg" class="me-3 rounded-circle" style="width:45px; height:45px;">
+                <div class="body">
+                    <h5 class="fw-bold">
+                        Anonymous
+                        <small class="text-muted">April 14, 2023</small>
+                    </h5>
+                    <p class="mb-0">
+                        You're right about that!
+                    </p>
+                    <div class="mb-1">
+                        <button class="btn p-1">
+                            <span class="fa-regular fa-heart">
+                        </button>
+                        <button class="btn p-1">Reply</button>
+                    </div>
+                </div>
+            </div>
+            <div class="d-flex my-2">
+                <img src="uploads/profile_pictures/anonymous.jpg" class="me-3 rounded-circle" style="width:45px; height:45px;">
+                <div class="body">
+                    <h5 class="fw-bold">
+                        Anonymous
+                        <small class="text-muted">April 14, 2023</small>
+                    </h5>
+                    <p class="mb-0">
+                        Wow, this is an awesome article!
+                    </p>
+                    <div class="mb-1">
+                        <button class="btn p-1">
+                            <span class="fa-regular fa-heart">
+                        </button>
+                        <button class="btn p-1">Reply</button>
+                    </div>
+                </div>
+            </div>
+            <div class="d-flex">
+                <div class="mx-3 ps-1 vr"></div>
+                <img src="uploads/profile_pictures/anonymous.jpg" class="me-3 rounded-circle" style="width:45px; height:45px;">
+                <div class="body">
+                    <h5 class="fw-bold">
+                        Anonymous
+                        <small class="text-muted">April 14, 2023</small>
+                    </h5>
+                    <p class="mb-0">
+                        You're right about that!
+                    </p>
+                    <div class="mb-1">
+                        <button class="btn p-1">
+                            <span class="fa-regular fa-heart">
+                        </button>
+                        <button class="btn p-1">Reply</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
