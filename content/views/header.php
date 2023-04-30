b3f614350d6949cccab50832179f0b62d7d10b8f
Logan Finley <finley4.logan@gmail.com>
2023-04-30 13:48:58 -0700

    LF Add a profile picture icon

--- content/views/header.php
+++ content/views/header.php
@@ -6,13 +6,11 @@ include_once "../include/user_info.inc.php";
 
         <div class="collapse navbar-collapse order-1 order-md-0 col-4" id="navbar-collapsed-menu">
             <ul class="navbar-nav me-auto">
-                <?php /*
                 <li class="nav-item">
                     <a class="btn d-md-block d-none " data-bs-toggle="offcanvas" href="#offcanvas-sidebar" role="button" aria-controls="offcanvas-sidebar">
                         <span class="fa-solid fa-bars"></span>
                     </a>
                 </li>
-                */ ?>
                 <li class="nav-item">
                     <form method="get" action="./search.php" class="input-group rounded">
                         <button class="btn input-group-text rounded" type="submit">
@@ -30,7 +28,7 @@ include_once "../include/user_info.inc.php";
             </button>
         </div>
 
-            <a class="navbar-brand col-4 mx-auto text-center order-0 order-md-1" href="./index.php">BlogBase</a>
+        <a class="navbar-brand col-4 mx-auto text-center order-0 order-md-1" href="./index.php">BlogBase</a>
 
         <!-- necessary for center-spacing the logo on smaller screens -->
         <div class="col-4 p-0 d-md-none d-block">
@@ -39,39 +37,57 @@ include_once "../include/user_info.inc.php";
         <div class="collapse navbar-collapse order-2 col-4" id="navbar-collapsed-menu">
             <ul class="navbar-nav ms-auto">
                 <?php session_start() ?>
-                <?php if ($user_info['admin'] == true) { ?>
+                <?php if ($user_info == null) {?>
                 <li class="nav-item">
-                    <a class="nav-link" href="./admin.php">admin</a>
+                    <a class="btn btn-outline-dark mx-1" href="./login.php">login</a>
+                </li>
+                <li class="nav-item">
+                    <a class="btn btn-outline-primary mx-1" href="./register.php">register</a>
                 </li>
                 <?php } ?>
-                <?php if ($user_info['advertiser'] == true) { ?>
+                <?php if (isset($user_info['username'])) { ?>
                 <li class="nav-item">
-                    <a class="nav-link" href="./ads.php">ads</a>
-                </li>
-                <?php } ?>
-                <?php if ($user_info['writer'] == true) { ?>
-                <li class="nav-item">
-                    <a class="nav-link" href="./writer.php">writer</a>
-                </li>
-                <?php } ?>
-                <?php if ($user_info['designer'] == true) { ?>
-                <li class="nav-item">
-                    <a class="nav-link" href="./design.php">design</a>
-                </li>
-                <?php } ?>
-                <?php if ($user_info['reader'] == true) { ?>
-                <li class="nav-item">
-                    <a class="nav-link" href="./saved.php">saved</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="../include/logout.inc.php">logout</a>
-                </li>
-                <?php } else {?>
-                <li class="nav-item">
-                    <a class="nav-link" href="./login.php">login</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="./register.php">register</a>
+                    <div class="dropdown">
+                        <button class="btn" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <img src="./uploads/profile_pictures/anonymous.jpg" class="rounded-circle" style="width:35px; height:35px;">
+                        </button>
+                        <ul class="dropdown-menu dropdown-menu-end">
+                            <li>
+                                <a class="dropdown-item" href="./profile.php">
+                                    <?php echo $user_info['first_name'] . " " . $user_info['last_name'] ?><br>
+                                    <?php echo "@" . $user_info['username'] ?>
+                                </a>
+                            </li>
+                            <hr>
+                            <li>
+                                <a class="dropdown-item" href="./saved.php">Saved Articles</a>
+                            </li>
+                            <?php if ($user_info['admin'] == true) { ?>
+                            <li>
+                                <a class="dropdown-item" href="./admin.php">Admin Dashboard</a>
+                            </li>
+                            <?php } ?>
+                            <?php if ($user_info['advertiser'] == true) { ?>
+                            <li>
+                                <a class="dropdown-item" href="./ads.php">Advertiser Dashboard</a>
+                            </li>
+                            <?php } ?>
+                            <?php if ($user_info['writer'] == true) { ?>
+                            <li>
+                                <a class="dropdown-item" href="./writer.php">Writer Dashboard</a>
+                            </li>
+                            <?php } ?>
+                            <?php if ($user_info['designer'] == true) { ?>
+                            <li>
+                                <a class="dropdown-item" href="./design.php">Designer Dashboard</a>
+                            </li>
+                            <?php } ?>
+                            <hr>
+                            <li>
+                                <a class="dropdown-item" href="../include/logout.inc.php">Logout</a>
+                            </li>
+                        </ul>
+                    </div>
                 </li>
                 <?php } ?>
             </ul>
