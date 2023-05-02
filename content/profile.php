c955e8f5ca32d8a32d7cefaa540672fd1f685fa8
Logan Finley <finley4.logan@gmail.com>
2023-05-02 16:58:33 -0700

    LF Show saved articles and written articles on profile pages

--- content/profile.php
+++ content/profile.php
@@ -50,6 +50,7 @@ if (isset($_GET['username'])) {
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
         <link rel="stylesheet" href="./css/styles.css">
 
+        <script src="./js/save_article.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
         <script src="https://code.jquery.com/jquery-3.6.4.min.js" integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8=" crossorigin="anonymous"></script>
 
@@ -112,10 +113,137 @@ if (isset($_GET['username'])) {
                     <h5 class="px-2 text-xxl-start text-center">
                         <?php echo $user_prof['biography']; ?>
                     </h5>
-
                     <hr>
+
+                    <?php
+                    $published_query = "SELECT Articles.article_id, Articles.title, Articles.description, Articles.lead_image,
+                                        Articles.content, UNIX_TIMESTAMP(Articles.submit_date) as submit_date,
+                                        Articles.approved, Articles.submitted, Articles.author_id
+                                        FROM Users join Articles on Users.user_id = Articles.author_id
+                                        WHERE Users.user_id = " . $user_prof['user_id'] . " AND Articles.approved = 1";
+                    $published_result = mysqli_query($con, $published_query);
+
+                    ?>
+
+                    <ul class="nav nav-tabs" id="users-tab" role="tablist">
+                        <?php if ($published_result->num_rows > 0) {?>
+                            <li class="nav-item" role="presentation">
+                                <button class="nav-link active" id="published-tab" data-bs-toggle="tab" data-bs-target="#published-tab-pane" type="button" role="tab" aria-controls="published-tab-pane" aria-selected="true">Published Articles</button>
+                            </li>
+                            <li class="nav-item" role="presentation">
+                                <button class="nav-link" id="saved-tab" data-bs-toggle="tab" data-bs-target="#saved-tab-pane" type="button" role="tab" aria-controls="saved-tab-pane" aria-selected="false">Saved Articles</button>
+                            </li>
+                        <?php } else { ?>
+                            <li class="nav-item" role="presentation">
+                                <button class="nav-link active" id="saved-tab" data-bs-toggle="tab" data-bs-target="#saved-tab-pane" type="button" role="tab" aria-controls="saved-tab-pane" aria-selected="false">Saved Articles</button>
+                            </li>
+                        <?php } ?>
+                    </ul>
+
+                    <div class="tab-content" id="users-tab-content">
+
+                        <?php if ($published_result->num_rows > 0) {?>
+                            <div class="tab-pane fade show active" id="published-tab-pane" role="tabpanel" aria-labelledby="published-tab" tabindex="0">
+                        <?php } else { ?>
+                            <div class="tab-pane fade" id="published-tab-pane" role="tabpanel" aria-labelledby="published-tab" tabindex="0">
+                        <?php } ?>
+                                <table class="table text-start">
+                                    <tbody>
+                                        <?php while ($current_row = $published_result->fetch_assoc()) { ?>
+                                            <tr>
+                                                <td class="text-nowrap">
+                                                    <?php echo date("M. d, Y", $current_row['submit_date']) ?>
+                                                </td>
+                                                <td>
+                                                    <h4>
+                                                        <a class="text-reset text-decoration-none" href="./article.php?id=<?php echo $current_row['article_id'] ?>">
+                                                            <?php echo $current_row['title'] ?>
+                                                        </a>
+                                                    </h4>
+                                                    <?php echo $current_row['description'] ?>
+                                                </td>
+                                            </tr>
+                                        <?php } ?>
+                                    </tbody>
+                                </table>
+                            </div>
+
+                        <?php
+                        $saved_articles_query = "SELECT UNIX_TIMESTAMP(saved_date) as saved_date, title, article_id, description
+                                                 FROM Articles NATURAL JOIN SavedArticles
+                                                 WHERE user_id = '" . $user_prof['user_id'] . "' AND approved = true";
+                        $saved_articles_result = mysqli_query($con, $saved_articles_query);
+                        ?>
+
+                        <?php if ($published_result->num_rows > 0) {?>
+                            <div class="tab-pane fade" id="saved-tab-pane" role="tabpanel" aria-labelledby="saved-tab" tabindex="0">
+                        <?php } else { ?>
+                            <div class="tab-pane fade show active" id="saved-tab-pane" role="tabpanel" aria-labelledby="saved-tab" tabindex="0">
+                        <?php } ?>
+                                <table class="table text-start">
+                                    <thead>
+                                    </thead
+                                    <tbody>
+                                        <?php if ($saved_articles_result->num_rows == 0) { ?>
+                                            <tr>
+                                                <h4 class="m-2">No saved articles...</h4>
+                                            </tr>
+                                        <?php } else {?>
+                                            <?php while ($current_row = $saved_articles_result->fetch_assoc()) { ?>
+                                                <tr id="article-<?php echo $current_row['article_id'] ?>">
+                                                    <td class="text-nowrap">
+                                                        <?php echo date("M. d, Y", $current_row['saved_date']) ?>
+                                                    </td>
+                                                    <td>
+                                                        <h4>
+                                                            <a class="text-reset text-decoration-none" href="./article.php?id=<?php echo $current_row['article_id'] ?>">
+                                                                <?php echo $current_row['title'] ?>
+                                                            </a>
+                                                        </h4>
+                                                        <?php echo $current_row['description'] ?>
+                                                    </td>
+                                                    <td>
+                                                        <?php
+                                                        if (isset($_SESSION['user_info'])) {
+                                                            $saved_query = "SELECT article_id, user_id
+                                                                            FROM SavedArticles
+                                                                            WHERE user_id = '" . $user_info['user_id'] . "' AND article_id = '" . $current_row['article_id'] . "';";
+                                                            $saved_result = mysqli_query($con, $saved_query); ?>
+                                                            <button onclick="change_bookmark_icon(<?php echo $current_row['article_id'] ?>)" id="bookmark-<?php echo $current_row['article_id'] ?>" class="btn p-1">
+                                                            <?php if ($saved_result->num_rows == 1) { ?>
+                                                                <span id="bookmark-icon-<?php echo $current_row['article_id'] ?>" class="fa-solid fa-bookmark">
+                                                            <?php } else { ?>
+                                                                <span id="bookmark-icon-<?php echo $current_row['article_id'] ?>" class="fa-regular fa-bookmark">
+                                                            <?php } ?>
+                                                            </button>
+                                                        <?php } else { ?>
+                                                            <a href="./login.php" id="bookmark" class="btn p-1">
+                                                                <span id="bookmark-icon" class="fa-regular fa-bookmark">
+                                                            </a>
+                                                        <?php } ?>
+                                                    </td>
+                                                </tr>
+                                            <?php } ?>
+                                        <?php } ?>
+                                    </tbody>
+                                </table>
+                            </div>
                 </div>
             </div>
         </div>
+        <script>
+        $(document).ready(function() {
+            $('button[data-bs-toggle="tab"]').on('click', function(e) {
+                localStorage.setItem('activeTab', e.target.dataset.bsTarget);
+            });
+            var activeTab = localStorage.getItem('activeTab');
+            if (activeTab) {
+                const triggerEl = document.querySelector(`button[data-bs-target="${activeTab}"]`);
+                if (triggerEl) {
+                    bootstrap.Tab.getOrCreateInstance(triggerEl).show()
+                }
+            }
+        });
+        </script>
     </body>
 </html/>
