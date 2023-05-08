55a3376efd4286fbe68738554b862024fc212a33
Logan Finley <finley4.logan@gmail.com>
2023-05-07 21:08:30 -0700

    LF Stylize the login error page

--- content/login_error.php
+++ content/login_error.php
@@ -1,4 +1,13 @@
-<?php require_once "../include/connect.inc.php"; ?>
+<?php
+require_once "../include/connect.inc.php";
+include_once "../include/user_info.inc.php";
+
+ob_start();
+
+if ($user_info) {
+    header("Location: ./index.php");
+}
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -9,6 +18,8 @@
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
         <link rel="stylesheet" href="./css/styles.css">
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 
         <title>BlogBase</title>
     </head>
@@ -22,13 +33,15 @@
         </div>
 
         <br>
-        <center>
-            <h3>ERROR: Incorrect Username or Password<br>Or<br>You are trying to log into the system too soon after account creation.</h3>
-            <br>
-            <h3>Click <a href="./login.php">here</a> to login again<br>or<br>Click <a href="./index.php">here</a> to return to home.</h3>
-        </center>
+        <div class="mt-3 text-center">
+            <h1>Incorrect Username or Password</h1>
+            <h1>
+                <a class="btn btn-outline-primary" href="./login.php">Login again</a>
+            </h1>
+            <h1>
+                <a class="btn btn-outline-primary" href="./index.php">Return home</a>
+            </h1>
+        </div>
 
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
-        <script src="./js/script.js"></script>
     </body>
 </html>
