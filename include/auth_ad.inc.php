0890bb9258613720da7fe0c33b200e8c4ad9cc58
Logan Finley <finley4.logan@gmail.com>
2023-05-13 14:15:06 -0700

    LF Remove unused files

--- include/auth_ad.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-    session_start();
-
-    if(!isset($_SESSION["username"])) {
-        header("Location: ../content/ads.php");
-        header("Location: ../content/need_to_sign_in.php");
-        exit();
-    }
-?>
--- include/auth_session.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-    session_start();
-
-    if(!isset($_SESSION["username"])) {
-        header("Location: ../content/admin.php");
-        header("Location: ../content/need_to_sign_in.php");
-        exit();
-    }
-?>
