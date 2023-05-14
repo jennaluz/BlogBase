8cc1a17de692dabe025fe10ff034cef3b2417a92
jennaluz <jpura827@gmail.com>
2023-05-13 19:20:17 -0700

    JP Allow writers to see unsubmitted articles

--- content/article.php
+++ content/article.php
@@ -23,16 +23,19 @@ if ($article_info == null) {
     die();
 }
 
-// check if user is accessing unapproved article w/o designer role
-if ($article_info['approved'] != true && $user_info['designer'] != true) {
-    // send to "you don't have access" page
-    http_response_code(404);
-    include("./404.php");
-    die();
-}
-
 if ($user_info['user_id'] == $article_info['author_id']) {
     $writer = true;
+}
+
+// check if user can see unsubmitted version (i.e. is the writer)
+if ($article_info['submitted'] == false && $writer != true) {
+    // check if user is accessing unapproved article w/o designer role
+    if ($article_info['approved'] != true && $user_info['designer'] != true) {
+        // send to "you don't have access" page
+        http_response_code(404);
+        include("./404.php");
+        die();
+    }
 }
 
 $roles_arr = array(
