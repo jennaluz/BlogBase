7e8bcae02d2220ea6b4fcc9d116f6bfc7fb0dfa5
Logan Finley <finley4.logan@gmail.com>
2023-05-13 20:57:38 -0700

    Fix SQL injection in settings page

--- include/update_account.inc.php
+++ include/update_account.inc.php
@@ -8,24 +8,31 @@ if (isset($_POST['update_account'])) {
 
     if ($user_info['username'] != $username) {
         echo "cannot update";
+        header("Location: ./index.php");
+        ob_end_flush();
     }
 
+    $user_info_array = array();
     $first_name = stripslashes($_POST['first_name']);
     $first_name = mysqli_real_escape_string($con, $first_name);
+    array_push($user_info_array, $first_name);
 
     $last_name = stripslashes($_POST['last_name']);
     $last_name = mysqli_real_escape_string($con, $last_name);
+    array_push($user_info_array, $last_name);
 
     $email = stripslashes($_POST['email']);
     $email = mysqli_real_escape_string($con, $email);
+    array_push($user_info_array, $email);
 
     $biography = $_POST['biography'];
+    array_push($user_info_array, $biography);
 
     $update_query = "UPDATE Users
-                     SET first_name = '$first_name', last_name = '$last_name', email = '$email', biography = '$biography'
+                     SET first_name = ?, last_name = ?, email = ?, biography = ?
                      WHERE username = '$username'";
 
-    $update_result = mysqli_query($con, $update_query);
+    $update_result = mysqli_execute_query($con, $update_query, $user_info_array);
 
     if (isset($_POST['old_password'])) {
         $old_password = stripslashes($_POST['old_password']);
@@ -34,12 +41,13 @@ if (isset($_POST['update_account'])) {
         if (password_verify($old_password, $user_info['password'])) {
             $new_password = stripslashes($_POST['new_password']);
             $new_password = mysqli_escape_string($con, $new_password);
+            $new_password = password_hash($new_password, PASSWORD_DEFAULT);
 
             $password_query = "UPDATE Users
-                               SET password = '" . password_hash($new_password, PASSWORD_DEFAULT) . "'
+                               SET password = ?
                                WHERE username = '$username'";
 
-            $password_result = mysqli_query($con, $password_query);
+            $password_result = mysqli_execute_query($con, $password_query, [$new_password]);
         }
     }
 
@@ -55,10 +63,10 @@ if (isset($_POST['update_account'])) {
         if (in_array($file_type, $accepted_types)) {
             if (move_uploaded_file($tmp_filename, $dest_filepath)) {
                 $prof_pic_query = "UPDATE Users
-                                   SET profile_picture = '$filename'
+                                   SET profile_picture = ?
                                    WHERE username = '$username'";
 
-                $result = mysqli_query($con, $prof_pic_query);
+                $result = mysqli_execute_query($con, $prof_pic_query, [$filename]);
             }
         }
     } else if ($_POST['profile-picture'] == 0) {
